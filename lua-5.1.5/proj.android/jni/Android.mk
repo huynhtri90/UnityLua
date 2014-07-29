3c4b1ef655f0a328e9264982ad322f1bdb1d54e9
dpull <beingstudio@gmail.com>
2014-07-29 19:47:03 +0800

    mk

--- lua-5.1.5/proj.android/jni/Android.mk
+++ lua-5.1.5/proj.android/jni/Android.mk
@@ -6,14 +6,21 @@ LOCAL_MODULE := lua
 
 LOCAL_MODULE_FILENAME := liblua
 
-LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/../src/*.c)
-LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../src/*.cpp)
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/../../src/*.c)
+LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/../../src/*.cpp)
 LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 
-# $(info SRC= $(LOCAL_SRC_FILES))
+$(info LOCAL_PATH= $(LOCAL_PATH))
+$(info LOCAL_SRC_FILES= $(LOCAL_SRC_FILES))
+
+LOCAL_SRC_FILES := $(subst  ../../src/lua.c,,$(LOCAL_SRC_FILES)) 
+LOCAL_SRC_FILES := $(subst  ../../src/luac.c,,$(LOCAL_SRC_FILES)) 
+$(info LOCAL_SRC_FILES= $(LOCAL_SRC_FILES))
 
 # LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include/lua
 
 LOCAL_CFLAGS += -DLUA_USE_LINUX
+
+# LOCAL_LDLIBS += -lreadline
 
 include $(BUILD_SHARED_LIBRARY)
